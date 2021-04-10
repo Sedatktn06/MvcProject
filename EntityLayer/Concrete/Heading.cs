36ed5c525afb1d4ea5135b74493d5b59cb7147ef
SedatKotan06@gmail.com <SedatKotan06@gmail.com>
2021-04-10 19:43:53 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Heading
+    {
+        public int HeadingId { get; set; }
+        public string HeadingName { get; set; }
+        public DateTime HeadingDate { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        public int WriterId { get; set; }
+        public string WriterName { get; set; }
+        public string WriterSurname { get; set; }
+        public string WriterImage { get; set; }
+        public string WriterMail { get; set; }
+        public string WriterPassword { get; set; }
+    }
+}
