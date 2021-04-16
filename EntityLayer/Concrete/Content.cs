d5b6808561d32d23c9dc3ece97c16d9694e9c1e3
SedatKotan06@gmail.com <SedatKotan06@gmail.com>
2021-04-17 02:28:12 +0300

    Database added with code first approach.

--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -18,8 +18,8 @@ namespace EntityLayer.Concrete
         public int HeadingID { get; set; }
         public virtual Heading Heading { get; set; }
 
-        public int WriterID { get; set; }
-        public virtual Writer Writer { get; set; }
+        //public int WriterID { get; set; }
+        //public virtual Writer Writer { get; set; }
 
     }
 }
