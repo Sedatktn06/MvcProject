6d2312601c3437cfcf7fa628676a055704eb40f4
SedatKotan06@gmail.com <SedatKotan06@gmail.com>
2021-04-24 08:59:38 +0300

    Category Manager Added

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        ICategoryDal _categoryDal;
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+        public void CategoryAdd(Category category)
+        {
+            if (category.CategoryName=="" || category.CategoryName.Length<=3 || category.CategoryDescription=="" || category.CategoryName>=51)
+            {
+                //Hata Mesajı
+            }
+            else
+            {
+                _categoryDal.Insert(category);
+            }
+        }
+    }
+}
--- DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -10,55 +10,58 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete.Repositories
 {
-    public class CategoryRepository : ICategoryDal
+    public class CategoryRepository : GenericRepository<Category>,ICategoryDal
     {
-        
-        DbSet<Category> _object;
 
-        public void Delete(Category category)
-        {
-            using(Context context = new Context())
-            {
-                _object.Remove(category);
-                context.SaveChanges();
-            }
-        }
 
-        public List<Category> Get(Expression<Func<Category, bool>> filter)
-        {
-            using(Context context=new Context())
-            {
-                throw new NotImplementedException();
-            }
-        }
 
-       
+        //    DbSet<Category> _object;
 
-        public void Insert(Category category)
-        {
-            using(Context context = new Context())
-            {
-               _object.Add(category);
-               context.SaveChanges();
-            }
-           
-        }
+        //    public void Delete(Category category)
+        //    {
+        //        using(Context context = new Context())
+        //        {
+        //            _object.Remove(category);
+        //            context.SaveChanges();
+        //        }
+        //    }
 
-        public List<Category> GetAll()
-        {
-            using(Context context = new Context())
-            {
-                return _object.ToList();
-            }
-            
-        }
+        //    public List<Category> Get(Expression<Func<Category, bool>> filter)
+        //    {
+        //        using(Context context=new Context())
+        //        {
+        //            throw new NotImplementedException();
+        //        }
+        //    }
 
-        public void Update(Category category)
-        {
-            using (Context context = new Context())
-            {
-                context.SaveChanges();
-            }
-        }
+
+
+        //    public void Insert(Category category)
+        //    {
+        //        using(Context context = new Context())
+        //        {
+        //           _object.Add(category);
+        //           context.SaveChanges();
+        //        }
+
+        //    }
+
+        //    public List<Category> GetAll()
+        //    {
+        //        using(Context context = new Context())
+        //        {
+        //            return _object.ToList();
+        //        }
+
+        //    }
+
+        //    public void Update(Category category)
+        //    {
+        //        using (Context context = new Context())
+        //        {
+        //            context.SaveChanges();
+        //        }
+        //    }
+        //}
     }
 }
