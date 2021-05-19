f8fbdb5eb2209f0ddbd4ca5500091f6ff0dc81fd
Sedatktn06 <54412886+Sedatktn06@users.noreply.github.com>
2021-05-19 11:11:19 +0300

    Heading Service and Manager Added

---
+++ BusinessLayer/Concrete/HeadingManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class HeadingManager : IHeadingService
+    {
+
+        IHeadingDal _headingDal;
+
+        public HeadingManager(IHeadingDal headingDal)
+        {
+            _headingDal = headingDal;
+        }
+
+        public List<Heading> GetAll()
+        {
+            return _headingDal.GetAll();
+        }
+
+        public Heading GetById(int id)
+        {
+            return _headingDal.Get(h => h.HeadingID == id);
+        }
+
+        public void HeadingAdd(Heading heading)
+        {
+            _headingDal.Insert(heading);
+        }
+
+        public void HeadingDelete(Heading heading)
+        {
+            _headingDal.Delete(heading);
+        }
+
+        public void HeadingUpdate(Heading heading)
+        {
+            _headingDal.Update(heading);
+        }
+    }
+}
---
+++ MvcProject/Controllers/HeadingController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject.Controllers
+{
+    public class HeadingController : Controller
+    {
+
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        
+        public ActionResult Index()
+        {
+            var headingValues = headingManager.GetAll();
+            return View(headingValues);
+        }
+    }
+}
